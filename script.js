76d43d63576492545bf356c0defd9d964091a055
yinkadev <109293802+yinkadev@users.noreply.github.com>
2022-10-25 04:16:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggle(){
+     
+  let  popup=document.getElementById('popup');
+  popup.classList.toggle('active')
+
+  let blur= document.getElementById('blur');
+  blur.classList.toggle('active')
+ 
+  
+ }
